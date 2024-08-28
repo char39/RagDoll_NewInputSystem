9770752ee4ce75cd8dabd2b309c528371118dd4e
Cinnamon <seongsun8330@naver.com>
2024-08-28 11:56:44 +0900

    0828_파일 업로드

---
+++ Assets/Scripts/RagDollCtrl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollCtrl : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Rigidbody[] RagDoll_rb;
+    public Collider[] RagDoll_cols;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        RagDoll_rb = transform.GetChild(2).GetComponentsInChildren<Rigidbody>();
+        RagDoll_cols = transform.GetChild(2).GetComponentsInChildren<Collider>();
+        SetRagDoll(true);
+        StartCoroutine(WakeUpRagDoll());
+    }
+
+    /// <summary> RagDoll의 IsKinematic을 Off </summary>
+    void SetRagDoll(bool isEnable)
+    {
+        foreach (Rigidbody rb in RagDoll_rb)
+            rb.isKinematic = !isEnable;
+    }
+
+    IEnumerator WakeUpRagDoll()
+    {
+        SetRagDoll(false);
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<Animator>().enabled = false;
+        SetRagDoll(true);
+    }
+}
